904ec4fb5c441f7814d7942bf68140e918dbb77d
Nandor Dudas <nandor.dudas@gmail.com>
2023-07-06 12:18:40 +0000

    chore: update deps Fixes #23

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -4,22 +4,13 @@ import UnoCSS from 'unocss/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 
-// import Components from 'unplugin-vue-components/vite'
-
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 const { pathname: root } = new URL('./src/', import.meta.url)
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // INFO: dist/assets/firebase-42cbea2b.js  644.83 kB │ gzip: 147.91 kB
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
-        },
-      },
-    },
+    chunkSizeWarningLimit: 800,
     target: 'esnext',
   },
   plugins: [
@@ -40,7 +31,6 @@ export default defineConfig({
       ],
       vueTemplate: true,
     }),
-    // Components(),
     UnoCSS(),
     VueDevTools(),
   ],
